91aecd9ce7d3ecf39a154b5dc396ffb9eb12be03
Aswin S S <aswinartography18@gmail.com>
2025-11-15 08:37:20 +0530

    extract digits in reverse order and print that number in that number times.

--- number.py
+++ number.py
@@ -1,10 +1,18 @@
 def extractNumbersWithLoop(num):
     while num>0:
-        lastdigit=num%10
+        lastdigit=num%10       
         print(lastdigit)
         num=num//10
 
+def extractNumbersAndPrintNumberPattern(num):
+    while num>0:
+        lastdigit=num%10
+        for i in range (0,lastdigit):
+            print(lastdigit)     
+        num=num//10
 
-value=extractNumbersWithLoop(21323)
+
+
+value=extractNumbersAndPrintNumberPattern(24)
 
 print(f"Result :{value}")
